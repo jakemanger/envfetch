b504af875690615f0264c0cc63a870d56dd0b8d6
jakemanger <jakesmanger@gmail.com>
2023-08-30 12:25:48 +0800

    improve create_time_lags speed
    Unit: milliseconds
                                                                                                                                                                 expr
         x %>% create_time_lags(n_lag_range = c(time_rep$n_start, time_rep$n_end),      time_lag = time_rep$interval, relative_to_start = time_rep$relative_to_start)
     x %>% new_create_time_lags(n_lag_range = c(time_rep$n_start,      time_rep$n_end), time_lag = time_rep$interval, relative_to_start = time_rep$relative_to_start)
          min       lq     mean   median     uq       max neval
     269.9964 294.1791 353.0652 311.7875 343.92 1057.2120   100
     174.4466 187.0627 209.7437 193.5012 203.76  612.1789   100

--- R/create_time_lags.R
+++ R/create_time_lags.R
@@ -35,52 +35,56 @@ create_time_lags <- function(
   geometries_to_lag <- sf::st_geometry(x)
   times_to_lag <- x %>% dplyr::pull(time_column_name)
 
-  new_geometries_column <- sf::st_sfc(crs=sf::st_crs(sf::st_geometry(x)))
-  envfetch__original_time_column <- lubridate::interval()
-  new_time_column <- lubridate::interval()
-  lag_amount <- c()
-  row_num <- c()
-
   start_range <- seq(n_lag_range[1], n_lag_range[2], by = 1)
   start_range <- start_range[1:length(start_range)-1]
 
+  n_rows_to_add <- length(start_range)
+
+  new_geometries_column <- vector("list", n_rows_to_add)
+  envfetch__original_time_column <- vector('list', n_rows_to_add)
+  new_time_column <- vector('list', n_rows_to_add)
+  envfetch__original_time_column <- vector('list', n_rows_to_add)
+  lag_amount <- vector('list', n_rows_to_add)
+  row_num <- vector('list', n_rows_to_add)
+
+  if (!('row_num' %in% colnames(x))) {
+    x$row_num <- 1:nrow(x)
+    message('`row_num` was not found in the data, so adding it to easily link time lag rows with their original source row')
+  }
+
+
   timezone <- lubridate::tz(x %>% dplyr::pull(time_column_name) %>% lubridate::int_start())
 
+
+  if (relative_to_start) {
+    relative_time <- lubridate::int_start(times_to_lag)
+  } else {
+    relative_time <- lubridate::int_end(times_to_lag)
+  }
+
+  i <- 1
   for (start in start_range) {
     min <- start * time_lag
     max <- (start + 1) * time_lag
 
-    if (relative_to_start) {
-      relative_time <- lubridate::int_start(times_to_lag)
-    } else {
-      relative_time <- lubridate::int_end(times_to_lag)
-    }
+    new_time_column[[i]] <- lubridate::interval(
+      start=relative_time + min,
+      end=relative_time + max - lubridate::seconds(1),
+      tz=timezone
+    )
+    new_geometries_column[[i]] <- geometries_to_lag
+    envfetch__original_time_column[[i]] <- times_to_lag
+    lag_amount[[i]] <- rep(
+      paste(
+        abs(as.numeric(lubridate::as.period(min)) / (as.numeric(lubridate::days(1)))),
+        abs(as.numeric(lubridate::as.period(max)) / (as.numeric(lubridate::days(1)))),
+        sep='_'
+      ),
+      length(times_to_lag)
+    )
+    row_num[[i]] <- x$row_num
 
-    new_time_column <- c(
-      new_time_column,
-      lubridate::interval(
-        start=relative_time + min,
-        end=relative_time + max - lubridate::seconds(1),
-        tz=timezone
-      )
-    )
-    new_geometries_column <- c(new_geometries_column, geometries_to_lag)
-    envfetch__original_time_column <- c(envfetch__original_time_column, times_to_lag)
-    lag_amount <- c(
-      lag_amount,
-      rep(
-        paste(
-          abs(as.numeric(lubridate::as.period(min)) / (as.numeric(lubridate::days(1)))),
-          abs(as.numeric(lubridate::as.period(max)) / (as.numeric(lubridate::days(1)))),
-          sep='_'
-        ),
-        length(times_to_lag)
-      )
-    )
-    row_num <- c(
-      row_num,
-      x$row_num
-    )
+    i <- i + 1
   }
 
   x$envfetch__original_time_column <- x %>% dplyr::pull(time_column_name)
@@ -89,11 +93,11 @@ create_time_lags <- function(
   geometry_column_name = attr(x, "sf_column")
 
   x <- x %>% dplyr::add_row(
-    !!geometry_column_name := new_geometries_column,
-    !!time_column_name := new_time_column,
-    envfetch__original_time_column = envfetch__original_time_column,
-    lag_amount = lag_amount,
-    row_num = row_num
+    !!geometry_column_name := do.call(c, new_geometries_column),
+    !!time_column_name := do.call(c, new_time_column),
+    envfetch__original_time_column = do.call(c, envfetch__original_time_column),
+    lag_amount = unlist(lag_amount),
+    row_num = unlist(row_num)
   )
 
   return(x)
