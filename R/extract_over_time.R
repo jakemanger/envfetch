1e7456a958010e8331fcbb2f04ffb885d4fac783
Jake Manger <jakesmanger@gmail.com>
2024-08-20 11:19:43 +0800

    add override of varnames if it is different to names

--- R/extract_over_time.R
+++ R/extract_over_time.R
@@ -164,10 +164,10 @@ extract_over_time <- function(
     new_col_names <- col_names_from_names
     warning(paste('`varnames` in the raster is empty. Assuming these are:', paste(new_col_names, collapse=', ')))
   }
-  # if (!all(new_col_names == col_names_from_names) || length(unique(new_col_names)) != length(new_col_names)) {
-  #   new_col_names <- col_names_from_names
-  #   warning(paste('Correcting `varnames` to:', paste(new_col_names, collapse=', '), ' as `varnames` in the raster don\'t match the prefix found in `names`.'))
-  # }
+  if (!all(new_col_names == col_names_from_names) || length(unique(new_col_names)) != length(new_col_names)) {
+    new_col_names <- col_names_from_names
+    warning(paste('Correcting `varnames` to:', paste(new_col_names, collapse=', '), ' as `varnames` in the raster don\'t match the prefix found in `names`.'))
+  }
   # initialise variables with NA
   x[, new_col_names] <- NA
   multi_values_in_extraction_per_row <- any(table(extracted$ID) > 1)
